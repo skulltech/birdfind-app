ead554d8995a74228000b3fc5eb2bda1064af943
Sumit Ghosh <sumit@sumit-ghosh.com>
2022-11-24 23:44:37 +0530

    Create basic filter input page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,58 @@
-import { Text } from "@mantine/core";
+import { Group, List, Stack, TextInput } from "@mantine/core";
+import { getHotkeyHandler } from "@mantine/hooks";
+import { useEffect, useState } from "react";
 
 const Home = () => {
-  return <Text>Hello world!</Text>;
+  const [followedByAll, setFollowedByAll] = useState<string[]>([]);
+  const [followerOfAll, setFollowerOfAll] = useState<string[]>([]);
+
+  const [followedByInput, setFollowedByInput] = useState("");
+  const [followerOfInput, setFollowerOfInput] = useState("");
+
+  const handleFollowedByEnter = () => {
+    if (followedByInput.length) {
+      setFollowedByAll([...followedByAll, followedByInput]);
+      setFollowedByInput("");
+    }
+  };
+
+  const handleFollowerOfEnter = () => {
+    if (followerOfInput.length) {
+      setFollowerOfAll([...followerOfAll, followerOfInput]);
+      setFollowerOfInput("");
+    }
+  };
+
+  useEffect(() => {
+    console.log(followedByAll, followerOfAll);
+  }, [followedByAll, followerOfAll]);
+
+  return (
+    <Stack>
+      <List>
+        {followedByAll.map((x) => (
+          <List.Item key={x}>followed by: {x}</List.Item>
+        ))}
+        {followerOfAll.map((x) => (
+          <List.Item key={x}>follower of: {x}</List.Item>
+        ))}
+      </List>
+      <Group>
+        <TextInput
+          label="Followed by"
+          value={followedByInput}
+          onChange={(event) => setFollowedByInput(event.currentTarget.value)}
+          onKeyDown={getHotkeyHandler([["Enter", handleFollowedByEnter]])}
+        />
+        <TextInput
+          label="Follower of"
+          value={followerOfInput}
+          onChange={(event) => setFollowerOfInput(event.currentTarget.value)}
+          onKeyDown={getHotkeyHandler([["Enter", handleFollowerOfEnter]])}
+        />
+      </Group>
+    </Stack>
+  );
 };
 
 export default Home;
