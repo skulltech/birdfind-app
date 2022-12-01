4bdd4c1b99db45e909dd4fb40e0d2d75ebe93ff6
Sumit Ghosh <sumit@sumit-ghosh.com>
2022-12-01 15:10:21 +0530

    Add barebones job-queue project

---
+++ packages/job-queue/src/workers.ts
@@ -0,0 +1,13 @@
+import { Worker } from "bullmq";
+
+const worker = new Worker("foo", async (job) => {
+  console.log(job.data);
+});
+
+worker.on("completed", (job) => {
+  console.log(`${job.id} has completed!`);
+});
+
+worker.on("failed", (job, err) => {
+  console.log(`${job.id} has failed with ${err.message}`);
+});
