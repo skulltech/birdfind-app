166595f915b6046b9f05802ba7a79665262bed8e
Sumit Ghosh <sumit@sumit-ghosh.com>
2022-12-19 23:38:42 +0530

    Check if session is active in index.tsx and redirect

--- packages/webapp/pages/index.tsx
+++ packages/webapp/pages/index.tsx
@@ -2,11 +2,16 @@ import { useEffect, useState } from "react";
 import { UserTable } from "../components/UserTable/UserTable";
 import { useTwips } from "../components/TwipsProvider";
 import { Center, Container, LoadingOverlay, Stack, Text } from "@mantine/core";
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
+import { getUserProfile } from "../utils/supabase";
+import { useRouter } from "next/router";
 
 const Home = () => {
   const { user, addFilters, searchResults, searchLoading, filtersInvalid } =
     useTwips();
   const [initialFiltersLoading, setInitialFiltersLoading] = useState(false);
+  const supabase = useSupabaseClient();
+  const router = useRouter();
 
   // Add user's following on first load of page
   useEffect(() => {
@@ -18,6 +23,19 @@ const Home = () => {
 
     if (user?.twitter?.username) addInitialFilters();
   }, [user]);
+
+  // To check if user is signed out, to bypass middleware's limitation
+  useEffect(() => {
+    const loadUser = async () => {
+      const {
+        data: { session },
+      } = await supabase.auth.getSession();
+
+      if (!session) router.push("/auth/signin");
+    };
+
+    loadUser();
+  }, [router, supabase]);
 
   return filtersInvalid ? (
     <Container mt={100}>
