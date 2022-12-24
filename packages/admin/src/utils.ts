63f051c0ad3c5cd00915d8d7e443be70a9f94c67
Sumit Ghosh <sumit@sumit-ghosh.com>
2022-12-24 15:27:41 +0530

    Fix type errors

--- packages/admin/src/utils.ts
+++ packages/admin/src/utils.ts
@@ -1,22 +1,19 @@
 import { createClient } from "@supabase/supabase-js";
 import { Queue } from "bullmq";
 import { Client } from "twitter-api-sdk";
-import { UpdateRelationJobResult, UpdateRelationJobData } from "@twips/common";
+import { UpdateRelationJobData } from "@twips/common";
 
 export const supabase = createClient(
   process.env.SUPABASE_API_URL,
   process.env.SUPABASE_SERVICE_ROLE_KEY
 );
 
-export const queue = new Queue<UpdateRelationJobData, UpdateRelationJobResult>(
-  "update-relation",
-  {
-    connection: {
-      host: process.env.REDIS_HOST,
-      port: parseInt(process.env.REDIS_PORT),
-      password: process.env.REDIS_PASSWORD,
-    },
-  }
-);
+export const queue = new Queue<UpdateRelationJobData>("update-relation", {
+  connection: {
+    host: process.env.REDIS_HOST,
+    port: parseInt(process.env.REDIS_PORT),
+    password: process.env.REDIS_PASSWORD,
+  },
+});
 
 export const twitter = new Client(process.env.TWITTER_BEARER_TOKEN);
--- packages/webapp/utils/job-queue.ts
+++ packages/webapp/utils/job-queue.ts
@@ -1,13 +1,10 @@
-import { UpdateRelationJobResult, UpdateRelationJobData } from "@twips/common";
+import { UpdateRelationJobData } from "@twips/common";
 import { Queue } from "bullmq";
 
-export const queue = new Queue<UpdateRelationJobData, UpdateRelationJobResult>(
-  "update-relation",
-  {
-    connection: {
-      host: process.env.REDIS_HOST,
-      port: parseInt(process.env.REDIS_PORT),
-      password: process.env.REDIS_PASSWORD,
-    },
-  }
-);
+export const queue = new Queue<UpdateRelationJobData>("update-relation", {
+  connection: {
+    host: process.env.REDIS_HOST,
+    port: parseInt(process.env.REDIS_PORT),
+    password: process.env.REDIS_PASSWORD,
+  },
+});
