b26c8b2d82423d00fa2884c2477f0e20ef4e1af7
Sumit Ghosh <sumit@sumit-ghosh.com>
2023-01-08 16:42:40 +0530

    Update environment.d.ts of webapp

--- packages/webapp/environment.d.ts
+++ packages/webapp/environment.d.ts
@@ -2,15 +2,10 @@ declare namespace NodeJS {
   interface ProcessEnv {
     NEXT_PUBLIC_SUPABASE_URL: string;
     NEXT_PUBLIC_SUPABASE_ANON_KEY: string;
-
     SUPABASE_SERVICE_ROLE_KEY: string;
 
-    TWITTER_BEARER_TOKEN: string;
     TWITTER_CLIENT_ID: string;
     TWITTER_CLIENT_SECRET: string;
-
-    GOOGLE_CLIENT_ID: string;
-    GOOGLE_CLIENT_SECRET: string;
 
     OPENAI_API_KEY: string;
 
