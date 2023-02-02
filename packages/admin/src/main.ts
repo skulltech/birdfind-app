5e21d25fbdf280872f7e0a2e76350f2db6eea045
Sumit Ghosh <sumit@sumit-ghosh.com>
2023-02-02 11:10:39 +0530

    Remove code used for experimenting with the AI sort idea

--- packages/admin/src/main.ts
+++ packages/admin/src/main.ts
@@ -1,11 +1,6 @@
 import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
 import { loadEntities } from "./load-entities";
-import {
-  analyzeUser,
-  createLookupCircleJob,
-  iterateLookupCircleJob,
-} from "./lookup-circle";
 import { lookupTwitterUser } from "./lookup-twitter-user";
 
 // Monkeypatching types for JSON serialization
@@ -25,72 +20,25 @@ process.removeAllListeners("warning");
 yargs(hideBin(process.argv))
   .command({
     command: "lookup-twitter-user",
-    describe: "Lookup Twitter user by ID",
+    describe: "Lookup Twitter user by username",
     builder: (yargs) =>
-      yargs.positional("id", {
+      yargs.positional("username", {
         type: "string",
         demandOption: true,
-        description: "Twitter ID of the user",
+        description: "Username of the user",
       }),
     handler: async (argv) => {
-      const id = argv._[1] as string;
-      console.info("Looking up Twitter user", id);
-      await lookupTwitterUser(id);
+      const username = argv._[1] as string;
+      console.info("Looking up Twitter user", username);
+      await lookupTwitterUser(username);
       console.info("Finished looking up Twitter user");
       // Because it might hang
       process.exit();
     },
   })
   .command({
-    command: "iterate-lookup-circle",
-    describe: "Lookup Twitter circle by user",
-    builder: (yargs) =>
-      yargs.positional("jobId", {
-        type: "string",
-        demandOption: true,
-        description: "Job ID",
-      }),
-    handler: async (argv) => {
-      const jobId = argv._[1] as number;
-      await iterateLookupCircleJob(jobId);
-      // Because it might hang
-      process.exit();
-    },
-  })
-  .command({
-    command: "create-lookup-circle",
-    describe: "Lookup Twitter circle by user",
-    builder: (yargs) =>
-      yargs.positional("username", {
-        type: "string",
-        demandOption: true,
-        description: "Seed username",
-      }),
-    handler: async (argv) => {
-      const username = argv._[1] as string;
-      await createLookupCircleJob(username);
-      // Because it might hang
-      process.exit();
-    },
-  })
-  .command({
-    command: "analyze-user",
-    describe: "Analyze user's niche",
-    builder: (yargs) =>
-      yargs.positional("username", {
-        type: "string",
-        demandOption: true,
-      }),
-    handler: async (argv) => {
-      const username = argv._[1] as string;
-      await analyzeUser(username);
-      // Because it might hang
-      process.exit();
-    },
-  })
-  .command({
     command: "load-entities",
-    describe: "Analyze user's niche",
+    describe: "Load entities from CSV file to database",
     handler: async (argv) => {
       await loadEntities();
       // Because it might hang
